93073e563ce42b364c08ef44b573a7f6502ecb46
kimchi1999 <1757010034chi@ou.edu.vn>
2023-02-16 14:12:20 +0700

    Cap nhap form dang ky

--- MyApp/src/main/java/module-info.java
+++ MyApp/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.myapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
     opens com.mycompany.myapp to javafx.fxml;
     exports com.mycompany.myapp;
